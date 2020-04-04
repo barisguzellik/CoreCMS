1d0948ec7dfd8a1c9584ea432f37eecd3be16f42
Barış Güzellik <barisguzellik@gmail.com>
2020-04-05 01:14:03 +0300

    Pages & Dapper Test

--- CoreCMS/Pages/Index.cshtml.cs
+++ CoreCMS/Pages/Index.cshtml.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace CoreCMS
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public int CategoryCount { get; set; }
+        public int ProductCount { get; set; }
+        public int ServiceCount { get; set; }
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
         public void OnGet()
         {
-
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                connection.Open();
+                CategoryCount = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM Category");
+                ProductCount = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM Product");
+                ServiceCount = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM Service");
+            }
         }
     }
 }
