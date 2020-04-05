61ab76927447ffe3c8df908c8f40b429f517e419
Barış Güzellik <barisguzellik@gmail.com>
2020-04-05 14:28:27 +0300

    Category DataTables

--- CoreCMS/Pages/Category/Index.cshtml.cs
+++ CoreCMS/Pages/Category/Index.cshtml.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreCMS.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.Extensions.Options;
+using Dapper;
+using System.Text.Json;
 namespace CoreCMS
 {
     public class CategoryModel : PageModel
     {
+        public string con { get; set; }
+        public CategoryModel(IOptions<ConnectionString> _con)
+        {
+            con = _con.Value.Default;
+        }
+        public List<Category> Category { get; set; }
         public void OnGet()
         {
-
+            using (var connection = new SqlConnection(con))
+            {
+                connection.Open();
+                Category = connection.Query<Category>("SELECT * FROM Category").ToList();
+            }
         }
     }
 }
