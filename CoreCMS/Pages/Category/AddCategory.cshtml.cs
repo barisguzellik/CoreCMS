23d908ed88e2bfcc4cd5fb0ffad75d6cd8ab24ad
Barış Güzellik <barisguzellik@gmail.com>
2020-04-05 23:48:01 +0300

    Category > File Upload

--- CoreCMS/Pages/Category/AddCategory.cshtml.cs
+++ CoreCMS/Pages/Category/AddCategory.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreCMS.Model;
 using Dapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
@@ -14,10 +17,18 @@ namespace CoreCMS
     public class AddCategoryModel : PageModel
     {
         public string con { get; set; }
-        public AddCategoryModel(IOptions<ConnectionString> _con)
+        private IWebHostEnvironment _environment;
+        public AddCategoryModel(IOptions<ConnectionString> _con, IWebHostEnvironment environment)
         {
             con = _con.Value.Default;
+            _environment = environment;
         }
+
+
+
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+
         [BindProperty]
         public Category Category { get; set; }
         public List<Category> Categories { get; set; }
@@ -35,11 +46,24 @@ namespace CoreCMS
         public IActionResult OnPost()
         {
             var category = Category;
+
+            //file upload
+            if (Upload != null)
+            {
+                var fileName = Guid.NewGuid().ToString() + "-" + Upload.FileName;
+                var file = Path.Combine(_environment.WebRootPath, "uploads", fileName);
+                using (var fileStream = new FileStream(file, FileMode.Create))
+                {
+                    Upload.CopyTo(fileStream);
+                }
+                category.Image = fileName;
+            }
+
             //Create
             using (var connection = new SqlConnection(con))
             {
-                var sql = "INSERT INTO Category(CategoryName,[Index],Status,ParentCategoryId,CreatedDate) Values(@CategoryName,@Index,@Status,@ParentCategoryId,@CreatedDate)";
-                var param = new { CategoryName = category.CategoryName, Index = category.Index, Status = category.Status, ParentCategoryId = category.ParentCategoryId, CreatedDate = DateTime.Now };
+                var sql = "INSERT INTO Category(CategoryName,Image,[Index],Status,ParentCategoryId,CreatedDate) Values(@CategoryName,@Image,@Index,@Status,@ParentCategoryId,@CreatedDate)";
+                var param = new { CategoryName = category.CategoryName, Image = category.Image, Index = category.Index, Status = category.Status, ParentCategoryId = category.ParentCategoryId, CreatedDate = DateTime.Now };
                 connection.Execute(sql, param);
             }
 
--- CoreCMS/Pages/Category/EditCategory.cshtml.cs
+++ CoreCMS/Pages/Category/EditCategory.cshtml.cs
@@ -8,17 +8,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 using Dapper;
 using System.Data.SqlClient;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace CoreCMS
 {
     public class EditCategoryModel : PageModel
     {
         public string con { get; set; }
-        public EditCategoryModel(IOptions<ConnectionString> _con)
+        private IWebHostEnvironment _environment;
+        public EditCategoryModel(IOptions<ConnectionString> _con, IWebHostEnvironment environment)
         {
             con = _con.Value.Default;
+            _environment = environment;
         }
 
+        [BindProperty]
+        public IFormFile Upload { get; set; }
         [BindProperty]
         public Category Category { get; set; }
         public List<Category> Categories { get; set; }
@@ -49,6 +56,8 @@ namespace CoreCMS
 
         public IActionResult OnPostDelete()
         {
+
+
             if (!string.IsNullOrEmpty(Request.Query["id"]))
             {
                 var category = Category;
@@ -66,15 +75,28 @@ namespace CoreCMS
 
         public IActionResult OnPostUpdate()
         {
+            var category = Category;
+            var id = int.Parse(Request.Query["id"]);
+
+            //file upload
+            if (Upload != null)
+            {
+                var fileName = Guid.NewGuid().ToString() + "-" + Upload.FileName;
+                var file = Path.Combine(_environment.WebRootPath, "uploads", fileName);
+                using (var fileStream = new FileStream(file, FileMode.Create))
+                {
+                    Upload.CopyTo(fileStream);
+                }
+                category.Image = fileName;
+            }
+
+            //update
             if (!string.IsNullOrEmpty(Request.Query["id"]))
             {
-                var category = Category;
-                var id = int.Parse(Request.Query["id"]);
-
                 using (var connection = new SqlConnection(con))
                 {
-                    var sql = "UPDATE Category Set CategoryName=@CategoryName,[Index]=@Index,Status=@Status,ParentCategoryId=@ParentCategoryId,CreatedDate=@CreatedDate Where CategoryId=@CategoryId";
-                    var param = new { CategoryId = id, CategoryName = category.CategoryName, Index = category.Index, Status = category.Status, ParentCategoryId = category.ParentCategoryId, CreatedDate = DateTime.Now };
+                    var sql = "UPDATE Category Set CategoryName=@CategoryName,Image=@Image,[Index]=@Index,Status=@Status,ParentCategoryId=@ParentCategoryId,CreatedDate=@CreatedDate Where CategoryId=@CategoryId";
+                    var param = new { CategoryId = id, CategoryName = category.CategoryName, Image = category.Image, Index = category.Index, Status = category.Status, ParentCategoryId = category.ParentCategoryId, CreatedDate = DateTime.Now };
                     connection.Execute(sql, param);
                 }
             }
